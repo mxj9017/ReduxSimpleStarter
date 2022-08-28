61bf8166336cf16e53fca1d540005f39218187f3
Mehul Jain <mehuljain@Mehuls-MBP.attlocal.net>
2022-08-28 12:09:10 -0500

    Working youtube app- need tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const API_KEY = "AIzaSyCX5YpUxUVNmRJUZrPk547MUXWMpTT43VA";
 
 import React, {Component} from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -21,7 +22,7 @@ class App extends Component {
       selectedVideo: null
     };
 
-    this.videoSearch('john');
+    this.videoSearch('john oliver');
   }
 
   videoSearch(term) {
@@ -34,12 +35,13 @@ class App extends Component {
     });
   }
 
-
-
   render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
     return (
       <div>
-        <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange = {videoSearch}/>
         <VideoDetail video = {this.state.selectedVideo}/>
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
